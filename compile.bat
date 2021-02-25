38b45ea40becc173fb748474d1fe991f734dc613
R-Michi <71868435+R-Michi@users.noreply.github.com>
2021-02-25 09:16:58 +0100

    Delete compile.bat

--- compile.bat
+++
@@ -1,3 +0,0 @@
-mkdir obj
-g++ -O3 -Wall -std=c++17 -I./lib/glm -I./lib/stb_master -I./lib/glc-1.0.0 -c main.cpp -o obj/main.o -fopenmp
-g++ -L./lib/glm/lib -o ray_tracing.exe obj/main.o -lglm_static -fopenmp
